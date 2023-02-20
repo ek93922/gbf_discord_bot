7c7b6316cc9b1d49e4a3baeb006a88bd92f92f8b
ek93922 <108656846+ek93922@users.noreply.github.com>
2023-02-19 22:40:57 -0800

    Add files via upload

--- gbf_wiki.py
+++ gbf_wiki.py
@@ -7,15 +7,17 @@ import math
 import re
 import titlecase
 
+
 class GbfWiki:
     def __init__(self):
-        self.headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.3'}
-        self.url = 'https://gbf.wiki'
-    
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.3"
+        }
+
     def get_response():
-        response = requests.get('https://gbf.wiki')
+        response = requests.get("https://gbf.wiki")
         page_content = response.content
-        soup = BeautifulSoup(page_content, 'lxml')
+        soup = BeautifulSoup(page_content, "lxml")
 
         return soup
 
@@ -24,58 +26,59 @@ class GbfWiki:
         # Defines current time in form of Dynamic Timestamp
         gmt = time.gmtime()
         timestamp = calendar.timegm(gmt)
-        
+
         soup = GbfWiki.get_response()
         # Find and print all current events
-        for td in soup.findAll('td', {'style': 'vertical-align: top; text-align: center;'}):
+        for td in soup.findAll(
+            "td", {"style": "vertical-align: top; text-align: center;"}
+        ):
             cur_evt_list = []
-            for img in td.findAll('img'):
-                evt = img.get('alt')
+            for img in td.findAll("img"):
+                evt = img.get("alt")
 
                 # Prevents duplicate entries in case same event has multiple banners img
                 if evt not in cur_evt_list:
                     cur_evt_list.append(evt)
-            
+
             # Creates 2 lists that keeps track of start timestamps and end timestamps
             start_time = []
             end_time = []
-            for span in td.findAll('span', {'class': 'localtime'}):
-                start_time.append(span.get('data-start'))
-                end_time.append(span.get('data-end'))
+            for span in td.findAll("span", {"class": "localtime"}):
+                start_time.append(span.get("data-start"))
+                end_time.append(span.get("data-end"))
 
         return cur_evt_list, start_time, end_time, timestamp
 
-
     # * Upcoming Events *
     def up_event():
-        
+
         soup = GbfWiki.get_response()
         # gbf.wiki's upcoming event td is identified with the last value of "style="vertical-align: top;""
         style_list = []
 
         # gbf.wiki's table format <td style="vertical-align: top;">
-        for td in soup.find_all('td', {'style': 'vertical-align: top;'}):
+        for td in soup.find_all("td", {"style": "vertical-align: top;"}):
             style_list.append(td)
 
-        # Selects last td which contains upcoming event information as established previously 
-        up_evt = style_list[len(style_list)- 1]
+        # Selects last td which contains upcoming event information as established previously
+        up_evt = style_list[len(style_list) - 1]
         up_evt_list = []
-        
+
         # <a href="LINK TO NAME OF EVENT" title="NAME OF EVENT">
-        for a in up_evt.findAll('img'):
-            evt = a.get('alt')
+        for a in up_evt.findAll("img"):
+            evt = a.get("alt")
 
             # Prevents duplicate entries in case same event has multiple banners img
             if evt not in up_evt_list:
                 up_evt_list.append(evt)
-        
+
         # Every odd entry is start time, even entry is end time
         time = []
-        for span in up_evt.findAll('span', {'class': 'localtime'}):
-            datetime = span.get('data-time')
+        for span in up_evt.findAll("span", {"class": "localtime"}):
+            datetime = span.get("data-time")
             if datetime not in time:
                 time.append(datetime)
-        
+
         # Create a dictionary that uses event as key,
         # and a list of start and end date as values.
         up_evt_sch = {}
@@ -89,10 +92,12 @@ class GbfWiki:
             time.pop(0)
 
             up_evt_sch[entry_name] = up_evt_time
-            
+
         return up_evt_sch
-    
+
+    # Returns wiki page relevant to user's input
     def search(user_input):
+        # Replaces all " " with "%20" to satisfy gbf wiki opensearch api
         search = re.sub("\s+", "%20", user_input)
         try:
             search_api = f"https://gbf.wiki/api.php?action=opensearch&format=json&search={search}&redirects=resolve"
@@ -100,5 +105,6 @@ class GbfWiki:
             response = requests.get(search_api)
             responseData = response.json()
             return responseData[3][0]
+        # In case user's search does not bring up any links
         except:
             return "Try Again."
